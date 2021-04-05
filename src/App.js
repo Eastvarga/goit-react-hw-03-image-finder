2acb864a4443cbfd4a0af9fa049345638df8fcf8
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-05 22:29:15 +0300

    Create axios fetch onSubmit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import ImageGallery from "./components/ImageGallery";
 import ImageGalleryItem from "./components/ImageGalleryItem";
 import Button from "./components/Button";
 import Modal from "./components/Modal";
+import fetchImages from "./components/services/image-api";
+
 class App extends Component {
   state = {
     query: "",
+    images: [],
+    perPage: 9,
+    page: 1,
   };
+  componentDidUpdate(prevState) {
+    // if (this.state.query !== prevState.query) {
+    //   console.log(
+    //     "componentDidUpdate state change?=",
+    //     this.state.query !== prevState.query
+    //   );
+    // }
+  }
   formSubmit = ({ query }) => {
-    this.setState({ query: query });
+    if (this.state.query !== query) {
+      fetchImages(query, this.state).then(({ hits }) => {
+        // console.dir(hits);
+        this.setState({
+          query: query,
+          images: [...hits],
+        });
+      });
+    }
+    // this.setState({ query: query });
   };
 
   render() {
@@ -17,7 +39,14 @@ class App extends Component {
       <div className="App">
         <SearchBar onSubmit={this.formSubmit} />
         <ImageGallery>
-          <ImageGalleryItem />
+          {this.state.images.map(({ id, webformatURL, largeImageURL }) => (
+            <ImageGalleryItem
+              key={id}
+              id={id}
+              url={webformatURL}
+              modalUrl={largeImageURL}
+            />
+          ))}
         </ImageGallery>
         <Button />
         {/* <Modal /> */}
