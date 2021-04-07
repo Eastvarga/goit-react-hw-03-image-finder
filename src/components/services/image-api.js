c543f87250b75a76cad80ee70a35aaec962c00d7
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-07 22:51:07 +0300

    change api func

--- src/components/services/image-api.js
+++ src/components/services/image-api.js
@@ -5,7 +5,7 @@ const AUTH_TOKEN = "19923655-1a3b53a89179877892e074405";
 const filter = "image_type=photo&orientation=horizontal";
 axios.defaults.baseURL = "https://pixabay.com/api";
 
-const fetchImages = (query, { perPage, page }) => {
+const fetchImages = ({ query, perPage, page }) => {
   return axios
     .get(
       `/?key=${AUTH_TOKEN}&q=${query}&${filter}&per_page=${perPage}&page=${page}`
