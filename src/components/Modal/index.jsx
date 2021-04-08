402e4884bcb3c9bb49415a0a27915f97a5227a2a
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-08 23:44:03 +0300

    added portal for modal and styles

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,13 +1,33 @@
 import { Component } from "react";
+import { createPortal } from "react-dom";
 import { ModalStyle, Overlay } from "./styles.module.css";
+const modalRoot = document.getElementById("modal-root");
+
 class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener("keydown", this.toggleModalHandler);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.toggleModalHandler);
+  }
+  toggleModalHandler = (event) => {
+    // console.log("toggleModalActivate");
+    if (event.target === event.currentTarget) {
+      this.props.onClick();
+    }
+    if (event.code === "Escape") {
+      this.props.onClick();
+    }
+  };
   render() {
-    return (
-      <div className={Overlay}>
+    return createPortal(
+      <div className={Overlay} onClick={this.toggleModalHandler}>
         <div className={ModalStyle}>
-          <img src="" alt="" />
+          <img src={this.props.url} alt={this.props.url} />
         </div>
-      </div>
+      </div>,
+      modalRoot
     );
   }
 }
